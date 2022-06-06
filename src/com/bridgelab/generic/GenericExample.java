fd9a0df04f338ecfb2d31a2b6e21c4f9180de041
sarfarazkha <102785717+sarfarazkha@users.noreply.github.com>
2022-06-06 23:36:05 +0530

    given 3 integers find max

---
+++ src/com/bridgelab/generic/GenericExample.java
@@ -0,0 +1,16 @@
+package com.bridgelab.generic;
+
+public class GenericExample <E> {
+    E x;
+
+    public static void main(String[] args) {
+        E m1(E data){
+
+            System.out.println(x + "-------"+ data);
+            return x;
+        }
+
+
+    }
+
+}
---
+++ src/com/bridgelab/generic/MainClass.java
@@ -0,0 +1,11 @@
+package com.bridgelab.generic;
+
+import java.util.List;
+import java.util.ListIterator;
+
+public class MainClass {
+    public static void main(String[] args) {
+        GenericExample<String> obj1 = new GenericExample;
+        Li
+    }
+}
